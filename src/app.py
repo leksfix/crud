cacfe7c351afce342f03e928c6f2b9fccad52fee
alex <leksfix@gmail.com>
2025-06-07 18:35:17 +0200

    CRUD-create

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, request, url_for, abort
+import os
+
+from flask import (
+    Flask,
+    flash,
+    get_flashed_messages,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from repository import PostsRepository
+from validator import validate
+
 
 app = Flask(__name__)
-
-repo = PostsRepository(50)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
@@ -11,30 +22,43 @@ def index():
     return render_template('index.html')
 
 
-# BEGIN (write your solution here)
-@app.route('/posts')
-def posts_index():
-    page = int(request.args.get('page', 1))
-    posts = repo.content()[(page - 1) * 5: page * 5]
+@app.get('/posts')
+def posts_get():
+    repo = PostsRepository()
+    messages = get_flashed_messages(with_categories=True)
+    posts = repo.content()
     return render_template(
-        "posts/index.html",
+        'posts/index.html',
         posts=posts,
-        prev_page = f"{url_for('posts_index')}?page={max(page - 1, 1)}",
-        next_page = f"{url_for('posts_index')}?page={page + 1}"
-    )
+        messages=messages,
+        )
 
-@app.route('/posts/<slug>')
-def posts_show(slug):
-    post = repo.find(slug)
-    if not post:
-        abort(404)
+
+# BEGIN (write your solution here)
+@app.get('/posts/new')
+def posts_new():
+    post = {}
+    errors = []
     return render_template(
-        "posts/show.html",
-        post=post
-    )
+        'posts/new.html', 
+        post=post, 
+        errors=errors
+        )
+
+@app.post('/posts')
+def posts_post():
+    repo = PostsRepository()
+    post = request.form.to_dict()
+    errors = validate(post)
+    if errors:
+        return render_template(
+            'posts/new.html', 
+            post=post, 
+            errors=errors
+            ), 422
+    repo.save(post)
+    flash("Post has been created")
+    return redirect(url_for("posts_get"))
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return "Page not found", 404
 # END
--- src/repository.py
+++ src/repository.py
@@ -1,12 +1,31 @@
-from data import generate
+import json
+import sys
+import uuid
+from flask import session
 
 
-class PostsRepository:
-    def __init__(self, size):
-        self.posts = generate(size)
+class PostsRepository():
+    def __init__(self):
+        if 'posts' not in session:
+            session['posts'] = {}
 
     def content(self):
-        return self.posts
+        return session['posts'].values()
 
-    def find(self, slug):
-        return next((post for post in self.posts if slug == post['slug']), None)
+    def find(self, id):
+        try:
+            return session['posts'][id]
+        except KeyError:
+            sys.stderr.write(f'Wrong post id: {id}')
+            raise
+
+    def destroy(self, id):
+        del session['posts'][id]
+
+    def save(self, post):
+        if not (post.get('title') and post.get('body')):
+            raise Exception(f'Wrong data: {json.loads(post)}')
+        if not post.get('id'):
+            post['id'] = str(uuid.uuid4())
+        session['posts'][post['id']] = post
+        session['posts'] = session['posts']
