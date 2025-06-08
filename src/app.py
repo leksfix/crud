5fa3e0085f3d6f8e32777d1d6196160607fefa9f
alex <leksfix@gmail.com>
2025-06-08 18:16:35 +0200

    CRUD-delete

--- src/app.py
+++ src/app.py
@@ -1,14 +1,15 @@
-import os
-
 from flask import (
     Flask,
     flash,
     get_flashed_messages,
+    make_response,
     redirect,
     render_template,
     request,
     url_for,
 )
+import os
+
 from repository import PostsRepository
 from validator import validate
 
@@ -34,31 +35,75 @@ def posts_get():
         )
 
 
-# BEGIN (write your solution here)
-@app.get('/posts/new')
-def posts_new():
+@app.route('/posts/new')
+def new_post():
     post = {}
-    errors = []
+    errors = {}
     return render_template(
-        'posts/new.html', 
-        post=post, 
-        errors=errors
-        )
+        'posts/new.html',
+        post=post,
+        errors=errors,
+    )
+
 
 @app.post('/posts')
 def posts_post():
     repo = PostsRepository()
-    post = request.form.to_dict()
+    data = request.form.to_dict()
+    errors = validate(data)
+    if errors:
+        return render_template(
+            'posts/new.html',
+            post=data,
+            errors=errors,
+            ), 422
+    id = repo.save(data)
+    flash('Post has been created', 'success')
+    resp = make_response(redirect(url_for('posts_get')))
+    resp.headers['X-ID'] = id
+    return resp
+
+
+# BEGIN (write your solution here)
+@app.get('/posts/<id>/update')
+def post_update_get(id):
+    repo = PostsRepository()
+    data = repo.find(id)
+    if not data:
+        return "Page not found", 404
+    errors = {}
+    return render_template(
+        'posts/edit.html',
+        post=data,
+        errors=errors
+    )
+
+@app.post('/posts/<id>/update')
+def post_update(id):
+    repo = PostsRepository()
+    data = request.form.to_dict()
+    post = repo.find(id)
+    if not post:
+        return "Page not found", 404
+    post['title'] = data['title']
+    post['body'] = data['body']
     errors = validate(post)
     if errors:
         return render_template(
-            'posts/new.html', 
-            post=post, 
+            'posts/edit.html',
+            post=post,
             errors=errors
-            ), 422
+        ), 422
     repo.save(post)
-    flash("Post has been created")
-    return redirect(url_for("posts_get"))
+    flash('Post has been updated')
+    return redirect(url_for('posts_get'))
 
 
+@app.post('/posts/<id>/delete')
+def post_delete(id):
+    repo=PostsRepository()
+    repo.destroy(id)
+    flash('Post has been removed')
+    return redirect(url_for('posts_get'))
+    
 # END
